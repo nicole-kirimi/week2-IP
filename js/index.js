686ea156491b53ab1029e3255526452fb61a200f
nicole <nicolekirimi123@gmail.com>
2024-04-16 10:42:57 +0300

    add spacing

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ document.getElementById('akanForm').addEventListener('submit'), function(event) 
     event.preventDefault();
 }
 
-const dob = new Date(document.getElementById('dob').value);
-  const gender = document.getElementById('gender').value;
+const dob = new Date(document.getElementById('dob').value)
+
+const gender = document.getElementById('gender').value
