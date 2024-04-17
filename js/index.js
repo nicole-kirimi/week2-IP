846954c63b22a4c7fa0ebb881e98f45e6b655252
nicole <nicolekirimi123@gmail.com>
2024-04-17 09:20:31 +0300

    changes in index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-document.getElementById('akanForm').addEventListener('submit'), function(event) {
-    event.preventDefault();
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const calculateButton = document.getElementById('calculate button')
+    calculateButton.addEventListener('click', calculateAkanName)
+})
 
-const dob = new Date(document.getElementById('dob').value)
-
+function calculateAkanName() {
+const dob = document.getElementById('dob').value
 const gender = document.getElementById('gender').value
-
-
+}
